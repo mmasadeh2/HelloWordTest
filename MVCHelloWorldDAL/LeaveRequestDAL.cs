745fe89822397c8cb8b112f61a9a069fd6d1cb68
amer_ <amer_@DESKTOP-BVP3MQR>
2018-10-07 16:44:17 +0300

    sd

--- MVCHelloWorldDAL/LeaveRequestDAL.cs
+++ MVCHelloWorldDAL/LeaveRequestDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCHelloWorldEntity.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace MVCHelloWorldDAL
 {
     class LeaveRequestDAL
     {
-
+        MVCHelloWorldContext dbContext;
         #region Constructor
         public LeaveRequestDAL()
         {
